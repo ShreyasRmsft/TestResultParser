d584b9af410de3d59e9e2b43bdffec8c04a8167e
Abhishek Kumawat <abhishekkumawat23@gmail.com>
2018-11-02 15:33:35 +0530

    run summary parser basic structure

--- src/Agent.Plugins/Parser/Python/RunSummaryParser.cs
+++ src/Agent.Plugins/Parser/Python/RunSummaryParser.cs
@@ -1,13 +1,46 @@
 ﻿namespace Agent.Plugins.TestResultParser.Parser.Python
 {
+    using System;
     using Agent.Plugins.TestResultParser.TestResult.Models;
 
     internal class RunSummaryParser : IRunSummaryParser
     {
         /// <inheritdoc />
-        public TestRunSummary Parse(string data, TestRunSummary runSummary)
+        public TestRunSummary Parse(string data, TestRunSummary partialTestRunSummary)
         {
-            throw new System.NotImplementedException();
+            // Validate
+            if (data == null) { return null; }
+
+            // Parse
+            var partialSummaryExists = partialTestRunSummary != null;
+            TestRunSummary runSummary = partialSummaryExists ? partialTestRunSummary : new TestRunSummary();
+            var parsed = partialSummaryExists ?
+                ParseForSummaryEnd(data, runSummary) :
+                ParseForSummaryStart(data, runSummary);
+
+            return parsed ? runSummary : null;
+        }
+
+        /// <summary>
+        /// Parse input data for summary start.
+        /// </summary>
+        /// <param name="data">Data to be parsed.</param>
+        /// <param name="runSummary">Run summary.</param>
+        /// <returns>True if parsing successful.</returns>
+        private bool ParseForSummaryStart(string data, TestRunSummary runSummary)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Parse input data for summary end.
+        /// </summary>
+        /// <param name="data">Data to be parsed.</param>
+        /// <param name="runSummary">Run summary.</param>
+        /// <returns>True if parsing successful.</returns>
+        private bool ParseForSummaryEnd(string data, TestRunSummary runSummary)
+        {
+            throw new NotImplementedException();
         }
     }
 }
