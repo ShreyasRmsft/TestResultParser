d820f925190f8e43f2eb98d89d26984f89be5e75
Nitin Gurram <nigurr@microsoft.com>
2019-01-18 11:05:34 +0530

    Update ITestRunManager.cs

--- Agent.Plugins.Log.TestResultParser.Contracts/TestRunManager/ITestRunManager.cs
+++ Agent.Plugins.Log.TestResultParser.Contracts/TestRunManager/ITestRunManager.cs
@@ -16,7 +16,10 @@ namespace Agent.Plugins.Log.TestResultParser.Contracts
         /// </summary>
         /// <param name="testRun"></param>
         Task PublishAsync(TestRun testRun);
-
+        
+        /// <summary>
+        /// Signal to complete any pending operations and dispose
+        /// </summary>
         Task FinalizeAsync();
     }
 }
