985661d98e104aba0f2f8376cb534d5bc5b4c364
Shreyas R <shrer@microsoft.com>
2019-02-12 17:22:18 +0530

    made the run name prefix protected

--- Agent.Plugins.Log.TestResultParser.Contracts/TestResult/Models/TestRun.cs
+++ Agent.Plugins.Log.TestResultParser.Contracts/TestResult/Models/TestRun.cs
@@ -45,11 +45,6 @@ namespace Agent.Plugins.Log.TestResultParser.Contracts
         public int TestRunId { get; }
 
         /// <summary>
-        /// The prefix to be addded to the test run title
-        /// </summary>
-        public string RunNamePrefix { get; }
-
-        /// <summary>
         /// Collection of passed tests
         /// </summary>
         public List<TestResult> PassedTests { get; set; } = new List<TestResult>();
@@ -68,5 +63,10 @@ namespace Agent.Plugins.Log.TestResultParser.Contracts
         /// Summary for the test run
         /// </summary>
         public TestRunSummary TestRunSummary { get; set; } = new TestRunSummary();
+
+        /// <summary>
+        /// The prefix to be addded to the test run title
+        /// </summary>
+        protected string RunNamePrefix;
     }
 }
